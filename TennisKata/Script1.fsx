0b528a2a69734da24499ecfc93faf775ca568cfc
JUSTGIVING01\fnewick <JUSTGIVING01\fnewick>
2015-02-04 14:29:09 +0000

    Ganji commit - post save

--- TennisKata/Script1.fsx
+++ TennisKata/Script1.fsx
@@ -1,1 +1,19 @@
 ﻿
+type Player = PlayerA | PlayerB
+
+type Points = Love | Fifteen | Thirty | Forty
+
+type GameScore =
+    | Points of Points * Points
+    | Deuce
+    | Advantage of Player
+    | Game of Player
+
+let scorePoint currentScore player =
+    match currentScore, player with
+    | Points(Love, pointsB), PlayerA -> Points(Fifteen, pointsB)
+    | Points(Fifteen, pointsB), PlayerA -> Points(Thirty, pointsB)
+    | Points(Thirty, Forty), PlayerA -> Deuce
+    | Points(Thirty, pointsB), PlayerA -> Points(Forty, pointsB)
+    | Deuce, PlayerA -> Advantage PlayerA
+    | Advantage PlayerA, PlayerA -> Game PlayerA
